d0a112e7b85b3705f08cc7b63f9e28dd47f6db78
Kyle Bolton <latestthing@gmail.com>
2018-02-08 13:34:07 -0600

    using the magic of spring @Configuration and dependency injection, setting the port is now abstracted into a separate class.

--- src/main/java/Node/Application.java
+++ src/main/java/Node/Application.java
@@ -10,16 +10,6 @@ import java.util.Properties;
 public class Application {
 
     public static void main(String[] args) {
-        NodeConfigurator nodeConfigurator = new NodeConfigurator();
-        ThisNodeInfo thisNodeInfo = nodeConfigurator.getThisNodeInfo();
-        Properties properties = new Properties();
-        String portNumber = String.valueOf(thisNodeInfo.getPort());
-        properties.setProperty("server.port", portNumber);
-
-        new SpringApplicationBuilder()
-                .properties(properties)
-                .sources(Application.class)
-                .run(args);
-        //SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
