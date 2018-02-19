7ce0720d2f7cd85f3ec187f891d45da9de8b1e6c
Kyle Bolton <latestthing@gmail.com>
2018-02-19 15:01:52 -0600

    moved wait for nodes to spin up to main method

--- src/main/java/Node/Application.java
+++ src/main/java/Node/Application.java
@@ -1,17 +1,24 @@
 package Node;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(Application.class, args);
         //ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         RootController rootController = context.getBean(RootController.class);
+        try{
+            Thread.sleep(30000);
+        }catch(InterruptedException e){
+            log.warn();
+        }
         rootController.sendLeaderElection();
     }
 }
