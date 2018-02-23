7532c1627acfdc5c88d651c1ca2db22f3169feb7
Kyle Bolton <latestthing@gmail.com>
2018-02-23 10:21:01 -0600

    added leader announce message

---
+++ src/main/java/Node/LeaderAnnounceMessage.java
@@ -0,0 +1,22 @@
+package Node;
+
+public class LeaderAnnounceMessage extends NodeMessage {
+    private int leaderUid;
+}
+    public LeaderAnnounceMessage() {
+        super();
+    }
+
+    public LeaderAnnounceMessage(int sourceUID, int targetUID, int leaderUid) {
+        super(sourceUID, targetUID);
+        this.leaderUid = leaderUid;
+    }
+
+    public int getLeaderUid() {
+        return leaderUid;
+    }
+
+    public void setLeaderUid(int leaderUid) {
+        this.leaderUid = leaderUid;
+    }
+}
