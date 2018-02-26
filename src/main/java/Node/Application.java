2364ee93b4a44def3c9af51e2e3083858c358068
Priyanka Goyal <priyankagoyal2093@gmail.com>
2018-02-26 12:52:23 -0600

    added config reading

--- src/main/java/Node/Application.java
+++ src/main/java/Node/Application.java
@@ -17,7 +17,7 @@ import org.springframework.core.io.Resource;
 @Slf4j
 public class Application {
     public static void main(String[] args) {
-        log.trace("--------before running application");
+
         ApplicationContext context = SpringApplication.run(Application.class, args);
         int numberOfNodes;
         log.trace("--------before running application");
