6c8abe5d6420783182be0defeb8792ad2c4e5077
Kyle Bolton <latestthing@gmail.com>
2018-02-26 21:02:03 -0600

    moved config reading to NodeConfigurator and improved style

--- src/main/java/Node/NodeConfigurator.java
+++ src/main/java/Node/NodeConfigurator.java
@@ -1,44 +1,110 @@
 package Node;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.*;
 
 @Configuration
 @Slf4j
 public class NodeConfigurator {
-    @Value("${this.port:3332}")
-    private int thisPort;
+    @Value("${this.hostName:'localhost'}")
+    private String thisHostName;
 
-    @Value("${neighbor.port:5678}")
-    private int neighborPort;
-
-    @Value("${this.uid:123}")
-    private int thisUid;
-
-    @Value("${neighbor.uid:123}")
-    private int neighborUid;
+    @Value("${nodeConfigUri:'file:resources/config.txt'}")
+    private String nodeConfigUri;
 
     @Bean
     @Qualifier("Node/NodeConfigurator/thisNodeInfo")
-    public ThisNodeInfo getThisNodeInfo() throws UnknownHostException{
-        String hostName = InetAddress.getLocalHost().getHostName();
-        //TODO remove
-        log.error("hostname: " + hostName);
+    public ThisNodeInfo getThisNodeInfo(
+        @Autowired ApplicationContext context
+    ) throws UnknownHostException{
+//        String hostName = InetAddress.getLocalHost().getHostName();
 
-        //TODO read from config
-        hostName = "localhost";
+        NodeConfig nodeConfig = readNodeConfig(context, thisHostName);
+        //TODO remove this hardcoding
+        thisHostName = "localhost";
 
-        ThisNodeInfo thisNodeInfo = new ThisNodeInfo(thisUid, hostName, thisPort);
+        int thisUid = nodeConfig.thisUid;
+        int thisPort = nodeConfig.nodes.get(thisUid).getPort();
+        ThisNodeInfo thisNodeInfo = new ThisNodeInfo(thisUid, thisHostName, thisPort);
 
-        thisNodeInfo.addNeighbor(new NodeInfo(neighborUid, "localhost", neighborPort));
+        nodeConfig.neighbors.forEach(neighborUid -> {
+            NodeInfo neighbor = nodeConfig.nodes.get(neighborUid);
+            thisNodeInfo.addNeighbor(neighbor);
+        });
 
         return thisNodeInfo;
     }
 
+    public NodeConfig readNodeConfig(ApplicationContext context, String thisNodeHostName){
+        Resource resource = context.getResource(nodeConfigUri);
+        Map<Integer, NodeInfo> nodes = new HashMap<>();
+        List<Integer> neighbors = new ArrayList<>();
+        String line;
+        int count = 0;
+        Integer thisNodeUid = new Integer(null);
+        try(
+                InputStream is = resource.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        ){
+            //throw away first line
+            br.readLine();
+            int numberOfNodes = Integer.parseInt(br.readLine());
+            while ((line = br.readLine()) != null) {
+                if (!line.startsWith("#") && !line.matches("^\\s*\\R")) {
+                    Queue<String> words = new LinkedList<>(Arrays.asList(line.split("\\s+")));
+                    if(count < numberOfNodes){
+                        int uid = Integer.parseInt(words.remove());
+                        String hostName = words.remove();
+                        int port = Integer.parseInt(words.remove());
+
+                        NodeInfo nodeInfo = new NodeInfo(uid, hostName, port);
+                        nodes.put(uid, nodeInfo);
+                        if(thisNodeHostName.equals(hostName)){
+                            thisNodeUid = uid;
+                        }
+                        count++;
+                    }
+
+                    if(
+                        count > numberOfNodes
+                        && count <= numberOfNodes * 2
+                        && thisNodeUid.equals(Integer.parseInt(words.remove()))
+                    ) {
+                        for (String word : words) {
+                            neighbors.add(Integer.parseInt(word));
+                        }
+                    }
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return new NodeConfig(thisNodeUid, nodes, neighbors);
+    }
+
+    private class NodeConfig {
+        private int thisUid;
+        private Map<Integer, NodeInfo> nodes;
+        private List<Integer> neighbors;
+
+        public NodeConfig(int thisUid, Map<Integer, NodeInfo> nodes, List<Integer> neighbors) {
+            this.thisUid = thisUid;
+            this.nodes = nodes;
+            this.neighbors = neighbors;
+        }
+    }
 }
