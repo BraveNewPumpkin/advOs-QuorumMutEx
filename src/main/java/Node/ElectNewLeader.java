6cec18c47044abdfc52855bad0b680c335ce4d00
Kyle Bolton <latestthing@gmail.com>
2018-02-27 14:33:06 -0600

    increased LeaderElection thread sleep to allow more time for other nodes' servers to spin up

--- src/main/java/Node/ElectNewLeader.java
+++ src/main/java/Node/ElectNewLeader.java
@@ -19,7 +19,7 @@ public class ElectNewLeader implements Runnable{
         //TODO: change to trace
         log.error("-------before sleep to allow other instances to spin up");
         try {
-            Thread.sleep(10 * 1000);
+            Thread.sleep(20 * 1000);
         } catch (InterruptedException e) {
             log.debug("thread interrupted!");
         }
