84739eabd7bc77fdbadee303399aa48df6718b34
Kyle Bolton <latestthing@gmail.com>
2018-04-04 11:57:24 -0500

    added Edge class to track edges on each node with their weights

---
+++ src/main/java/Node/Edge.java
@@ -0,0 +1,57 @@
+package Node;
+
+public class Edge implements Comparable<Edge> {
+    int firstUid, secondUid;
+    int weight;
+
+    public Edge() {
+    }
+
+    public Edge(int firstUid, int secondUid, int weight) {
+        this.firstUid = firstUid;
+        this.secondUid = secondUid;
+        this.weight = weight;
+    }
+
+    public int compareTo(Edge edge) {
+        if(weight > edge.getWeight()) {
+            return 1;
+        } else if(weight < edge.getWeight()) {
+            return -1;
+        } else if(firstUid > edge.getFirstUid()) {
+            return 1;
+        } else if(firstUid < edge.getFirstUid()) {
+            return -1;
+        } else if(secondUid > edge.getSecondUid()) {
+            return 1;
+        } else if(secondUid < edge.getSecondUid()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int getFirstUid() {
+        return firstUid;
+    }
+
+    public void setFirstUid(int firstUid) {
+        this.firstUid = firstUid;
+    }
+
+    public int getSecondUid() {
+        return secondUid;
+    }
+
+    public void setSecondUid(int secondUid) {
+        this.secondUid = secondUid;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
--- src/main/java/Node/ThisNodeInfo.java
+++ src/main/java/Node/ThisNodeInfo.java
@@ -1,30 +1,40 @@
 package Node;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class ThisNodeInfo extends NodeInfo{
-    List<NodeInfo> neighbors;
-    private int distanceToRoot;
+    private final List<NodeInfo> neighbors;
+    private final Map<NodeInfo, Edge> edges;
+    private final int totalNumberOfNodes;
 
-    ThisNodeInfo(int uid, String hostName, int port) {
+    ThisNodeInfo(int uid, int totalNumberOfNodes, String hostName, int port) {
         super(uid, hostName, port);
-        neighbors = new ArrayList<NodeInfo>();
+        this.totalNumberOfNodes = totalNumberOfNodes;
+        neighbors = new ArrayList<>();
+        edges = new HashMap<>();
     }
 
     public boolean addNeighbor(NodeInfo neighbor){
         return neighbors.add(neighbor);
     }
 
-    public int getDistanceToRoot() {
-        return distanceToRoot;
-    }
-
-    public void setDistanceToRoot(int distanceToRoot) {
-        this.distanceToRoot = distanceToRoot;
-    }
-
     public List<NodeInfo> getNeighbors() {
         return neighbors;
     }
+
+    public Edge addEdge(Edge edge, NodeInfo connectedNode){
+        return edges.put(connectedNode, edge);
+    }
+
+    public Map<NodeInfo, Edge> getEdges() {
+        return edges;
+    }
+
+    public int getTotalNumberOfNodes() {
+        return totalNumberOfNodes;
+    }
+
 }
