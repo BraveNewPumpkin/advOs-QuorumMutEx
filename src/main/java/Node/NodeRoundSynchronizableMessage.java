3aeffc3494dcb53484a9cd77f9f7a3cf916ee9e6
Kyle Bolton <latestthing@gmail.com>
2018-06-22 13:16:25 -0500

    copied classes for round synchronization of messages from prev project

---
+++ src/main/java/Node/NodeRoundSynchronizableMessage.java
@@ -0,0 +1,29 @@
+package Node;
+
+public class NodeRoundSynchronizableMessage extends NodeMessage implements RoundSynchronizable {
+    private int roundNumber;
+
+    public NodeRoundSynchronizableMessage() {
+        super();
+    }
+
+    public NodeRoundSynchronizableMessage(int sourceUID, int roundNumber) {
+        super(sourceUID);
+        this.roundNumber = roundNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "NodeRoundSynchronizableMessage{" +
+                "roundNumber=" + roundNumber +
+                "} " + super.toString();
+    }
+
+    public int getRoundNumber() {
+        return roundNumber;
+    }
+
+    public void setRoundNumber(int roundNumber) {
+        this.roundNumber = roundNumber;
+    }
+}
---
+++ src/main/java/Node/RoundSynchronizable.java
@@ -0,0 +1,7 @@
+package Node;
+
+public interface RoundSynchronizable {
+    public int getRoundNumber();
+
+    public void setRoundNumber(int roundNumber);
+}
